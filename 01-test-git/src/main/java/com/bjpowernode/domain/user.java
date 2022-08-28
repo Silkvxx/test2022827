632a3bedf0b59121167a87cd6e8e4b3101e73de0
zs <zs@bjpowernode.com>
2022-08-28 11:12:07 +0800

    main方法

--- 01-test-git/src/main/java/com/bjpowernode/domain/user.java
+++ 01-test-git/src/main/java/com/bjpowernode/domain/user.java
@@ -5,4 +5,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class user {
     private String username;
+
+    public static void main(String[] args) {
+            int i = 1;
+            while(i>0){
+                System.out.println("i==="+i++);
+            }
+    }
 }
